ca38bb9f1c89be49dc3d74a20fcfb5c4ff0c725a
Rafael Abensur <rafael@abensur.me>
2025-05-09 23:47:30 -0300

    fix: netlify settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,33 +2,21 @@ export default defineNuxtConfig({
   future: {
     compatibilityVersion: 4,
   },
-
   css: ['~/assets/css/main.css'],
-
   pages: true,
-
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui-pro',
     '@nuxt/image',
     '@nuxt/content'
   ],
-
   runtimeConfig: {
     public: {
       builderApiKey: process.env.BUILDER_PUBLIC_KEY
     }
   },
-
-  nitro: {
-    preset: 'netlify'
-  },
-
-
   typescript: {
     strict: true,
     typeCheck: false
   },
-
-  compatibilityDate: '2023-11-04'
 })
